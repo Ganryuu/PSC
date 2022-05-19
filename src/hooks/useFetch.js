77a697372f52333e6517617fd91ad5fcb9558e7f
Hichem Fantar <hichemfantar2049@gmail.com>
2022-05-19 22:17:57 +0100

    it works?

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,46 +1,52 @@
 import axios from "axios";
 import { useState, useEffect, useCallback } from "react";
 
-const useFetch = (url, page = "", limit = "", isEnabled = true) => {
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [error, setError] = useState(null);
+const useFetch = (
+	url,
+	defaultState,
+	page = "",
+	limit = "",
+	isEnabled = true
+) => {
+	const [data, setData] = useState(defaultState || null);
+	const [isPending, setIsPending] = useState(true);
+	const [isSuccess, setIsSuccess] = useState(false);
+	const [error, setError] = useState(null);
 
-  const fetch = useCallback(
-    async (search = "") => {
-      setIsPending(true);
-      setIsSuccess(false);
-      setError(null);
-      axios
-        .get(
-          `${url}?${page && `page=${page}&`}${limit && `limit=${limit}&`}${
-            search && `search=${search}&`
-          }`
-        )
-        .then((res) => {
-          return res.data;
-        })
-        .then((data) => {
-          setIsSuccess(true);
-          setData(data);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.response.data);
-        })
-        .finally(() => {
-          setIsPending(false);
-        });
-    },
-    [limit, page, url]
-  );
+	const fetch = useCallback(
+		async (search = "") => {
+			setIsPending(true);
+			setIsSuccess(false);
+			setError(null);
+			axios
+				.get(
+					`${url}?${page && `page=${page}&`}${limit && `limit=${limit}&`}${
+						search && `search=${search}&`
+					}`
+				)
+				.then((res) => {
+					return res.data;
+				})
+				.then((data) => {
+					setData(data);
+					setIsSuccess(true);
+					setError(null);
+				})
+				.catch((err) => {
+					setError(err.response.data);
+				})
+				.finally(() => {
+					setIsPending(false);
+				});
+		},
+		[limit, page, url]
+	);
 
-  useEffect(() => {
-    fetch();
-  }, [fetch]);
+	useEffect(() => {
+		fetch();
+	}, [fetch]);
 
-  return { data, setData, isPending, error, fetch, isSuccess };
+	return { data, setData, isPending, error, fetch, isSuccess };
 };
 
 export default useFetch;
--- src/pages/products.js
+++ src/pages/products.js
@@ -4,43 +4,63 @@ import { products } from "../__mocks__/products";
 import { ProductListToolbar } from "../components/product/product-list-toolbar";
 import { ProductCard } from "../components/product/product-card";
 import { DashboardLayout } from "../components/dashboard-layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useFetch from "src/hooks/useFetch";
 
-const Products = () => (
-	<>
-		<Head>
-			<title>Products</title>
-		</Head>
-		<Box
-			component="main"
-			sx={{
-				flexGrow: 1,
-				py: 8,
-			}}
-		>
-			<Container maxWidth={false}>
-				<ProductListToolbar />
-				<Box sx={{ pt: 3 }}>
-					<Grid container spacing={3}>
-						{products.map((product) => (
-							<Grid item key={product.id} lg={4} md={6} xs={12}>
-								<ProductCard product={product} />
-							</Grid>
-						))}
-					</Grid>
-				</Box>
-				<Box
-					sx={{
-						display: "flex",
-						justifyContent: "center",
-						pt: 3,
-					}}
-				>
-					<Pagination color="primary" count={3} size="small" />
-				</Box>
-			</Container>
-		</Box>
-	</>
-);
+// async function getProducts() {
+// 	try {
+// 		const res = await axios.get("/dilevry-actions");
+// 		return res.data;
+// 	} catch (err) {
+// 		return [];
+// 	}
+// }
+
+const Products = () => {
+	const fetchUsersQuery = useFetch(`/dilevry-actions`, [], "", 100);
+
+	// useEffect(() => {
+	// 	setProducts(fetchUsersQuery.data);
+	// }, []);
+
+	return (
+		<>
+			<Head>
+				<title>Products</title>
+			</Head>
+			<Box
+				component="main"
+				sx={{
+					flexGrow: 1,
+					py: 8,
+				}}
+			>
+				<Container maxWidth={false}>
+					<ProductListToolbar />
+					<Box sx={{ pt: 3 }}>
+						<Grid container spacing={3}>
+							{fetchUsersQuery.data.map((product) => (
+								<Grid item key={product?.id} lg={4} md={6} xs={12}>
+									<ProductCard product={product} />
+								</Grid>
+							))}
+						</Grid>
+					</Box>
+					{/* <Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							pt: 3,
+						}}
+					>
+						<Pagination color="primary" count={3} size="small" />
+					</Box> */}
+				</Container>
+			</Box>
+		</>
+	);
+};
 
 Products.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
